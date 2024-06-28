68e8c31eefe4322823906f54397e029b435cc141
kimkimyeye <kimkimye@naver.com>
2024-06-28 16:05:50 +0900

    240628 모바일, 타블렛 버전 추가

--- index.js
+++ index.js
@@ -21,23 +21,35 @@ let mainSwiper = new Swiper(".mainSwiper", {
 
 // New Arrivals Swiper
 let newArrivalsSwiper = new Swiper(".new-arrivals .swiper", {
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 20,
   loop: true,
   navigation: {
     nextEl: ".new-arrivals .swiper-button-next",
     prevEl: ".new-arrivals .swiper-button-prev",
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
 });
 
 // Most Loved Swiper
 let mostLovedSwiper = new Swiper(".most-loved .swiper", {
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 20,
   loop: true,
   navigation: {
     nextEl: ".most-loved .swiper-button-next",
     prevEl: ".most-loved .swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
   },
 });
 
@@ -60,84 +72,6 @@ let collectionSwiper = new Swiper(".collectionSwiper", {
     prevEl: ".collectionSwiper .swiper-button-prev",
   },
 });
-
-// 베가스 사용한 코드
-  // $(document).ready(function () {
-  //   $('.background1').hover(function () {
-  //     $('.event-list').vegas({
-  //       delay: 1500,
-  //       timer: false,
-  //       // firstTransition: 'fade',
-  //         slides: [
-  //             { src: './assets/images/image/event-img1.jpg' },
-  //             { src: './assets/images/image/event-img1-2.jpg' },
-  //             { src: './assets/images/image/event-img1-3.jpg' },
-  //             { src: './assets/images/image/event-img1-4.jpg' }
-  //         ]
-  //     });
-  // });
-  
-  // $('.background1').mouseout(function () {
-  //     $('.event-list').vegas('destroy');
-  // });
-
-    
-  //   $('.background2').hover(function () {
-  //     $('.event-list').vegas({
-  //       delay: 1500,
-  //       timer: false,
-  //       // firstTransition: 'fade',
-  //         slides: [
-  //             { src: './assets/images/image/event-img4.jpg' },
-  //             { src: './assets/images/image/event-img3.jpg' },
-  //             { src: './assets/images/image/event-img2.jpg' },
-  //             { src: './assets/images/image/event-img1.jpg' }
-  //         ]
-  //     });
-  // });
-  
-  // $('.background2').mouseout(function () {
-  //     $('.event-list').vegas('destroy');
-  // });
-
-    
-  //   $('.background3').hover(function () {
-  //     $('.event-list').vegas({
-  //       timer: false,
-  //       delay: 1500,
-  //       // firstTransition: 'fade',
-  //         slides: [
-  //             { src: './assets/images/image/event-img1.jpg' },
-  //             { src: './assets/images/image/event-img2.jpg' },
-  //             { src: './assets/images/image/event-img3.jpg' },
-  //             { src: './assets/images/image/event-img4.jpg' }
-  //         ]
-  //     });
-  // });
-  
-  // $('.background3').mouseout(function () {
-  //     $('.event-list').vegas('destroy');
-  // });
-
-    
-  //   $('.background4').hover(function () {
-  //     $('.event-list').vegas({
-  //       timer: false,
-  //       delay: 1500,
-  //       // firstTransition: 'fade',
-  //         slides: [
-  //             { src: './assets/images/image/event-img2.jpg' },
-  //             { src: './assets/images/image/event-img3.jpg' },
-  //             { src: './assets/images/image/event-img1.jpg' },
-  //             { src: './assets/images/image/event-img2.jpg' }
-  //         ]
-  //     });
-  // });
-  
-  // $('.background4').mouseout(function () {
-  //     $('.event-list').vegas('destroy');
-  // });
-// });
 
   // 베가스 안쓴 코드
   $(document).ready(function () {
